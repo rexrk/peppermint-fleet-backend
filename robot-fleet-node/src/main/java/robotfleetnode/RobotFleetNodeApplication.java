6ec1843fa54131024c6f542bb0d061a572f8ab9d
rexrk <kraman938@gmail.com>
2026-09-02 13:38:46 +0530

    Adds properties structure

--- robot-fleet-node/src/main/java/robotfleetnode/RobotFleetNodeApplication.java
+++ robot-fleet-node/src/main/java/robotfleetnode/RobotFleetNodeApplication.java
@@ -1,4 +1,4 @@
-package io.github.rexrk.robotfleetnode;
+package robotfleetnode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- robot-fleet-node/src/test/java/robotfleetnode/RobotFleetNodeApplicationTests.java
+++ robot-fleet-node/src/test/java/robotfleetnode/RobotFleetNodeApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.rexrk.robotfleetnode;
+package robotfleetnode;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
