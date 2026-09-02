acc7e0e7221a031d51bd7a169f63abb0f7119e98
rexrk <kraman938@gmail.com>
2026-09-02 14:46:03 +0530

    Integrates outbound mqtt

--- robot-fleet-node/src/main/java/robotfleetnode/config/MqttConfig.java
+++ robot-fleet-node/src/main/java/robotfleetnode/config/MqttConfig.java
@@ -2,10 +2,15 @@ package robotfleetnode.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.mqtt.core.ClientManager;
+import org.springframework.integration.mqtt.core.Mqttv3ClientManager;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 
 @Configuration
 @RequiredArgsConstructor
@@ -15,11 +20,28 @@ public class MqttConfig {
     private final RobotProperties robotProperties;
     private final MqttProperties mqttProperties;
 
+    // Same Client for both inbound and outbound channels
     @Bean
-    public IntegrationFlow mqttInbound() {
+    public ClientManager<IMqttAsyncClient, MqttConnectOptions> mqttClientManager() {
 
-        String clientId = mqttProperties.getClientId()
-                .formatted(robotProperties.getId());
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setServerURIs(
+                new String[]{mqttProperties.getBrokerUrl()}
+        );
+
+        options.setCleanSession(mqttProperties.isCleanSession());
+
+        return new Mqttv3ClientManager(
+                options,
+                mqttProperties.getClientId()
+                        .formatted(robotProperties.getId())
+        );
+    }
+
+    @Bean
+    public IntegrationFlow mqttInbound(
+            ClientManager<IMqttAsyncClient, MqttConnectOptions> clientManager) {
 
         String commandTopic = mqttProperties.getTopics()
                 .getRobotCommand()
@@ -27,8 +49,7 @@ public class MqttConfig {
 
         return IntegrationFlow
                 .from(new MqttPahoMessageDrivenChannelAdapter(
-                        mqttProperties.getBrokerUrl(),
-                        clientId,
+                        clientManager,
                         commandTopic
                 ))
                 .handle(message -> {
@@ -36,4 +57,36 @@ public class MqttConfig {
                 })
                 .get();
     }
+
+    @Bean
+    public IntegrationFlow mqttOutbound(
+            ClientManager<IMqttAsyncClient, MqttConnectOptions> clientManager) {
+
+        String statusTopic = mqttProperties.getTopics()
+                .getRobotStatus()
+                .formatted(robotProperties.getId());
+
+        MqttPahoMessageHandler handler =
+                new MqttPahoMessageHandler(clientManager);
+
+        handler.setDefaultTopic(statusTopic);
+        handler.setDefaultQos(mqttProperties.getQos());
+
+        return IntegrationFlow
+                .from("mqttOutboundChannel")
+                .handle(handler)
+                .get();
+    }
+
+    /* Outbounnd test
+    @Bean
+    CommandLineRunner testOutbound(MessageChannel mqttOutboundChannel) {
+        return args -> mqttOutboundChannel.send(
+                MessageBuilder.withPayload(
+                        "{\"status\":\"R1 ONLINE\"}"
+                ).build()
+        );
+    }
+
+     */
 }
